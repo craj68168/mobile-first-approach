a2829ba7015582ca4aff0aa7b4572988360807f0
Raj Chaudhary <craj68168@gmail.com>
2023-09-28 13:56:37 +0545

    feat:nav section for mobile done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import Head from "next/head";
-import "./styles.css"
+import "./styles.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [showClass, setShowClass] = useState(false);
+  const toggleClassName = () => {
+    setShowClass(!showClass);
+  };
   return (
     <>
       <Head>
@@ -11,12 +17,36 @@ export default function Home() {
         <div className="logo-section">
           <img className="logo" src="./img/logo.png" alt="logo" />
           <div className="search-block">
-          <input type="text" className="input" placeholder="Please enter the text.." />
-          <img src="./img/search-icon.png" alt="search-icon"/>
+            <input
+              type="text"
+              className="input"
+              placeholder="Please enter the text.."
+            />
+            <img src="./img/search-icon.png" alt="search-icon" />
+          </div>
         </div>
+        <div className="hamburger" onClick={() => toggleClassName()}>
+          <img src="./img/hamburger.png" alt="hamburger" />
         </div>
-       
-        <img src="./img/hamburger.png" alt="hamburger" />
+        <ul className={showClass ? "show" : "hide"}>
+          <li>
+            <a href="#">Become a Host</a>
+          </li>
+          <li>
+            <a href="#">Trips</a>
+          </li>
+          <li>
+            <a href="#">Messages</a>
+          </li>
+          <li>
+            <a href="#">Help</a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="./img/avatar.png" alt="avatar" />
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
